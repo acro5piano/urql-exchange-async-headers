a4b3c8d00d22193388eb5e37854ad00cdd4ca8f2
Kay Gosho <ketsume0211@gmail.com>
2022-06-02 14:59:12 +0900

    init

---
+++ src/index.ts
@@ -0,0 +1,36 @@
+import { Exchange } from 'urql'
+import { fromPromise, map, mergeMap, pipe } from 'wonka'
+
+type GetHeaders = () => Promise<Record<string, string>>
+
+export const asyncHeaderExchange =
+  (getHeaders: GetHeaders): Exchange =>
+  ({ forward }) =>
+  (operations$) =>
+    pipe(
+      operations$,
+      mergeMap((operation) => {
+        return pipe(
+          fromPromise(getHeaders()),
+          map((headers) => {
+            if (!operation.context.fetchOptions) {
+              operation.context.fetchOptions = {
+                headers: {},
+              }
+            }
+            if (typeof operation.context.fetchOptions === 'function') {
+              console.warn(
+                '[urql-exchange-async-headers] no headers are attached because `operation.context.fetchOptions` is a function',
+              )
+              return operation
+            }
+            Object.assign(operation.context.fetchOptions.headers!, {
+              ...operation.context.fetchOptions.headers!,
+              ...headers,
+            })
+            return operation
+          }),
+        )
+      }),
+      forward,
+    )
