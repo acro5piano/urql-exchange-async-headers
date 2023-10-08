90726f90d7e41f379052f6079d93f05752949edc
Kay Gosho <ketsume0211@gmail.com>
2023-10-09 00:20:23 +0900

    feat: support URQL v4

--- tests/asyncHeaderExchange.test.ts
+++ tests/asyncHeaderExchange.test.ts
@@ -3,17 +3,28 @@ import 'isomorphic-unfetch'
 import test from 'ava'
 
 import { asyncHeaderExchange } from '../src'
-import { createClient, defaultExchanges } from 'urql'
+import { createClient, cacheExchange, fetchExchange } from 'urql'
 import getPort from 'get-port'
 import { createServer, IncomingMessage } from 'http'
 
 function serve(port: number, onReq: (req: IncomingMessage) => void) {
   const server = createServer((req, res) => {
+    console.log('requested!')
     res.end()
     onReq(req)
   })
   server.listen(port)
 }
+
+const exchanges = [
+  cacheExchange,
+  asyncHeaderExchange(async () => {
+    return {
+      foo: 'bar',
+    }
+  }),
+  fetchExchange,
+]
 
 test('it adds header asynchronously', (t) => {
   return new Promise(async (resolve) => {
@@ -25,25 +36,18 @@ test('it adds header asynchronously', (t) => {
 
     const client = createClient({
       url: `http://localhost:${port}/graphql`,
-      exchanges: [
-        asyncHeaderExchange(async () => {
-          return {
-            foo: 'bar',
-          }
-        }),
-        ...defaultExchanges,
-      ],
+      exchanges,
     })
 
-    await client.query(`{ hello }`).toPromise()
+    await client.query(`{ hello }`, {}).toPromise()
   })
 })
 
-test('it does not add header asynchronously if fetchOptions function defined', (t) => {
+test('it adds header asynchronously if fetchOptions function defined', (t) => {
   return new Promise(async (resolve) => {
     const port = await getPort()
     serve(port, (req) => {
-      t.is(req.headers['foo'], undefined)
+      t.is(req.headers['foo'], 'bar')
       t.is(req.headers['baz'], 'qux')
       resolve()
     })
@@ -57,17 +61,10 @@ test('it does not add header asynchronously if fetchOptions function defined', (
           },
         }
       },
-      exchanges: [
-        asyncHeaderExchange(async () => {
-          return {
-            foo: 'bar',
-          }
-        }),
-        ...defaultExchanges,
-      ],
+      exchanges,
     })
 
-    await client.query(`{ hello }`).toPromise()
+    await client.query(`{ hello }`, {}).toPromise()
   })
 })
 
@@ -87,16 +84,9 @@ test('it merges existing headers', (t) => {
           baz: 'qux',
         },
       },
-      exchanges: [
-        asyncHeaderExchange(async () => {
-          return {
-            foo: 'bar',
-          }
-        }),
-        ...defaultExchanges,
-      ],
+      exchanges,
     })
 
-    await client.query(`{ hello }`).toPromise()
+    await client.query(`{ hello }`, {}).toPromise()
   })
 })
